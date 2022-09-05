eec69aac1b7fe39c16561e6176210b396234e117
Kyle Rader <kylewrader@gmail.com>
2022-09-05 15:31:12 -0700

    Add some account seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+accounts = Account.create([
+    { email: "kylewrader@gmail.com", password: "astrongpassword", status: 2 },
+    { email: "user1@gmail.com", password: "astrongpassword", status: 2 },
+    { email: "user2@gmail.com", password: "astrongpassword", status: 2 },
+    { email: "user3@gmail.com", password: "astrongpassword", status: 2 },
+])
