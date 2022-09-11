02e5eecca7359034646151d7dd9e4cb894d4c4ef
Kyle Rader <kylewrader@gmail.com>
2022-09-11 16:26:20 -0700

    Fix seeds.rb Account creations

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
 accounts = Account.create([
-    { email: "kylewrader@gmail.com", password: "astrongpassword", status: 2 },
-    { email: "user1@gmail.com", password: "astrongpassword", status: 2 },
-    { email: "user2@gmail.com", password: "astrongpassword", status: 2 },
-    { email: "user3@gmail.com", password: "astrongpassword", status: 2 },
+    { email: "kylewrader@gmail.com", password: "astrongpassword", status: 2, alias: 'mcyamaha' },
+    { email: "user1@gmail.com", password: "astrongpassword", status: 2, alias: 'user1' },
+    { email: "user2@gmail.com", password: "astrongpassword", status: 2, alias: 'user2' },
+    { email: "user3@gmail.com", password: "astrongpassword", status: 2, alias: 'user3' },
 ])
