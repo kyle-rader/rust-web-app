81f59b71502a378cf430500e501e2dfa347dce90
Kyle Rader <kylewrader@gmail.com>
2022-09-11 22:34:10 -0700

    Add list of banned words

--- app/services/words.rb
+++ app/services/words.rb
@@ -1,12 +1,22 @@
 class Words
-    @@words_4 = File.new("app/assets/words/4.txt").readlines.map {|w| w.strip }
-    
-    def self.words_4
-        @@words_4
-    end
-    
-    def self.triplet
-        @@words_4.sample(3).join('-')
-    end
+  def self.scrub(word)
+    word
+    .gsub(/[\s\-_]+/, "") # sub space, dash, underscores with empty string
+    .downcase()
+  end
 
+  @@words_4 = File.new("app/assets/words/4.txt").readlines.map {|w| w.strip }
+  @@banned = Set.new(File.new("app/assets/words/banned.txt").readlines.map {|w| scrub(w.strip) })
+
+  def self.words_4
+    @@words_4
+  end
+
+  def self.triplet
+    @@words_4.sample(3).join('-')
+  end
+
+  def self.banned?(word, word_list = @@banned)
+    word_list.include?(scrub(word))
+  end
 end
---
+++ spec/services/words_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+TEST_WORDS = Set.new(["foo", "foobar"])
+
+RSpec.describe "Words" do
+  def subject(word)
+    Words.banned?(word, TEST_WORDS)
+  end
+
+  it { expect(subject("foo")).to be true }
+  it { expect(subject("FOo")).to be true }
+  it { expect(subject("foo bar")).to be true }
+  it { expect(subject(" foo bar  ")).to be true }
+  it { expect(subject("foo-bar")).to be true }
+  it { expect(subject("foo_bar")).to be true }
+
+  it { expect(subject("this_is_fine")).to be false }
+end
