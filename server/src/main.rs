c55dfa38982d90aa5a75c864b5dbf574ce1e445d
Kyle Rader <kylewrader@gmail.com>
2024-04-28 20:19:21 -0700

    Start axum web server with ctrl-c handler

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,59 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{http::StatusCode, routing::get, Router};
+use tokio::signal;
+use tracing_subscriber::fmt;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    #[cfg(debug_assertions)]
+    let startup_msg = "🐛 (debug) Starting automata server";
+    #[cfg(not(debug_assertions))]
+    let startup_msg = "🚀 (release) Starting automata server";
+    println!("{startup_msg}");
+
+    // initialize tracing
+    fmt::init();
+
+    // Build server
+    let app = Router::new().route("/", get(root));
+
+    // run app with hyper
+    let addr = "127.0.0.1:3000";
+    let listener = tokio::net::TcpListener::bind(addr).await?;
+
+    println!("Server running on: http://{}", addr);
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await?;
+
+    Ok(())
+}
+
+async fn root() -> (StatusCode, &'static str) {
+    (StatusCode::OK, "Hello, World!")
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("Error: failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    println!("💤 Shutting down...");
 }
