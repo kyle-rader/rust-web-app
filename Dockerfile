8f8d08a5705497edfcadf1289d5a6464549bd14a
Kyle Rader <kylewrader@gmail.com>
2024-05-05 14:47:56 -0700

    get automata app running from docker image

---
+++ Dockerfile
@@ -0,0 +1,42 @@
+# Use a small Linux-based image as the base
+FROM alpine:latest as builder
+
+# Set the working directory
+WORKDIR /app
+
+# Install necessary dependencies
+RUN apk update && \
+	apk add --no-cache nodejs npm rust cargo
+
+# Copy the frontend application source code
+COPY frontend /app/frontend
+
+# Build the frontend application
+RUN cd frontend && \
+	npm install && \
+	npm run build
+
+# Copy the server source code
+COPY server /app/server
+
+# Build the server
+RUN cd server && \
+	cargo build --release
+
+# Start fresh image to reduce size
+FROM alpine:latest
+RUN apk --no-cache add ca-certificates libgcc libstdc++
+
+WORKDIR /root/
+
+COPY --from=builder /app/server/target/release/server .
+
+# Expose the port the server listens on
+EXPOSE 3000
+
+# Set env vars for server configuration
+ENV PORT=3000
+ENV ADDRESS=0.0.0.0
+
+# Set the server binary as the entry point
+CMD ["./server"]
--- server/src/main.rs
+++ server/src/main.rs
@@ -4,6 +4,9 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 mod assets;
 
+const DEFAULT_PORT: u16 = 3000;
+const DEFAULT_ADDR: &str = "127.0.0.1";
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     #[cfg(debug_assertions)]
@@ -25,11 +28,16 @@ async fn main() -> anyhow::Result<()> {
         .route("/", get(assets::handler))
         .route("/*file", get(assets::handler));
 
-    // run app with hyper
-    let addr = "127.0.0.1:3000";
-    let listener = tokio::net::TcpListener::bind(addr).await?;
+    // get port from env or use default
+    let port = std::env::var("PORT").unwrap_or(DEFAULT_PORT.to_string());
+    let address = std::env::var("ADDRESS").unwrap_or(DEFAULT_ADDR.to_string());
 
-    println!("🛫 Server running on: http://{}", addr);
+    // Get a TCP listener ready to accept incoming connections
+    let address = format!("{address}:{port}");
+    let listener = tokio::net::TcpListener::bind(&address).await?;
+
+    // Start server
+    println!("🛫 Server running on: http://{}", address);
     axum::serve(listener, app)
         .with_graceful_shutdown(shutdown_signal())
         .await?;
