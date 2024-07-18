21e7e4a3f596fe222910f7f871a83a73101a551e
Kyle Rader <kylewrader@gmail.com>
2024-07-17 22:56:19 -0700

    add initial login page

--- client/src/routes/login/+page.svelte
+++ client/src/routes/login/+page.svelte
@@ -1,1 +1,21 @@
+<script lang="ts">
+	import LoginForm from './login_form.svelte';
+	import { post } from '$lib/requests';
+
+	async function login(event: any) {
+		let { email, password } = event.detail;
+		console.log('Logging in', email, password);
+
+		const res = await post('/api/login', { email, password });
+		if (res.ok) {
+			const { token } = await res.json();
+			console.log('Logged in!', token);
+		} else {
+			console.log('Login failed');
+		}
+	}
+</script>
+
 <h1 class="container center">Login</h1>
+
+<LoginForm on:login={login} />
---
+++ client/src/routes/login/login_form.svelte
@@ -0,0 +1,85 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	let email = '';
+	let password = '';
+	$: ready = email.length > 0 && password.length > 0;
+
+	function login() {
+		dispatch('login', { email, password });
+	}
+</script>
+
+<div class="container center max-w">
+	<form on:submit|preventDefault={login}>
+		<div class="form-item">
+			<label for="email">Email</label>
+			<input type="email" id="email" bind:value={email} required />
+		</div>
+		<div class="form-item">
+			<label for="password">Password</label>
+			<input type="password" id="password" bind:value={password} required />
+		</div>
+
+		<button class="button" class:ready type="submit">Login</button>
+	</form>
+</div>
+
+<style>
+	form {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		margin: 0 auto;
+	}
+
+	.form-item {
+		display: flex;
+		flex-direction: column;
+		margin-bottom: 1.2rem;
+	}
+
+	label {
+		text-align: left;
+		font-size: 1.2rem;
+		margin-bottom: 0.5rem;
+	}
+
+	input {
+		padding: 0.5rem;
+		font-size: 1.1rem;
+		border-radius: 0.5rem;
+		border: none;
+	}
+
+	small {
+		margin-top: 0.5rem;
+		text-align: left;
+		font-size: 0.9rem;
+	}
+
+	.button {
+		padding: 0.5rem;
+		font-size: 1.2rem;
+		border-radius: 0.5rem;
+		border: none;
+		background-color: var(--light2);
+	}
+	.button.ready {
+		background-color: var(--green);
+	}
+	.button.ready::before {
+		content: '🚀';
+		margin-right: 0.5rem;
+	}
+	.button.ready::after {
+		content: '🎉';
+		margin-left: 0.5rem;
+	}
+
+	.max-w {
+		max-width: 380px;
+		margin: auto;
+	}
+</style>
