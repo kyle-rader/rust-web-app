170db8d16655e9b4a0c0e2816890631100cf3f08
Kyle Rader <kylewrader@gmail.com>
2024-11-16 21:54:55 -0800

    start media queries on homepage

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -3,57 +3,72 @@
 
 <h1 class="container center">Automata Games</h1>
 
-<div class="options center align-middle">
-	<div class="game-option">
+<div class="container options center">
+	<div class="option">
 		<h2>Join</h2>
 		<input type="text" placeholder="Game Code" />
 		<button class="button">Join Game</button>
 	</div>
-	<div class="game-option">
+	<div class="option">
 		<h2>Host</h2>
 		<button class="button">Create Lobby</button>
 	</div>
-	<div class="game-option">
+	<div class="option">
 		<h2>Play</h2>
 		<button class="button">Choose Game</button>
 	</div>
 </div>
 
 <style>
-	.options {
-		display: flex;
-		justify-content: center;
+	/* Mobile */
+	@media (max-width: 600px) {
+		.options {
+			width: 95%;
+			margin: auto;
+		}
+
+		.option {
+			display: block;
+			margin-top: 6px;
+		}
 	}
 
-	.game-option {
-		margin: 20px;
-		padding: 20px;
-		width: 200px;
-		justify-content: center;
+	/* Tablet */
+	@media (min-width: 601px) and (max-width: 1024px) {
+		.options {
+			width: 50%;
+		}
+		.option {
+			display: inline-block;
+		}
+	}
+
+	/* Desktop */
+	@media (min-width: 1025px) {
+		.options {
+			width: 30%;
+		}
+		.option {
+			display: inline-block;
+		}
+	}
+
+	button,
+	input {
+		width: 100%;
+		text-align: center;
+		border: none;
+		border-radius: 6px;
 	}
 
 	input {
-		text-align: center;
-		width: 100%;
-		margin: 12px 0 0 0;
-		padding: 8px 0px;
-		border: none;
-		border-radius: 6px;
-		font-size: 20px;
-		font-variant: small-caps;
+		/* font-variant: small-caps; */
 	}
 
 	button {
-		border: none;
 		color: var(--dark0);
 		background-color: var(--green);
-		width: 100%;
-		margin: 8px 0px;
-		padding: 12px 0px;
-		border-radius: 6px;
-		text-align: center;
 		text-decoration: none;
-		font-size: 18px;
 		cursor: pointer;
 	}
 </style>
